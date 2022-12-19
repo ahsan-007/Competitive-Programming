7608f1cc42a4ef728544a097d2bb2b8ed0178860
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2022-12-19 12:22:38 +0500

    Added a Function that Calculates Nth Fibonacci Number using Bottom Up Approach

--- FibonacciMemoized.py
+++ FibonacciMemoized.py
@@ -1,19 +1,33 @@
 
-def fibonacci(n, memo=[0, 1]):
+def fibonacci_top_down(n, memo=[0, 1]):
     if n <= len(memo):
         return memo[n - 1]
-    memo.append(fibonacci(n - 1, memo) + fibonacci(n - 2, memo))
+    memo.append(fibonacci_top_down(n - 1, memo) +
+                fibonacci_top_down(n - 2, memo))
     return memo[n - 1]
 
 
+def fibonacci_bottom_up(n):
+    if n <= 2:
+        return 1 if n == 2 else 0
+
+    a, b = 0, 1
+    while n > 2:
+        a, b = b, a+b
+        n = n - 1
+    return b
+
+
 def main():
-    print(fibonacci(1))
-    print(fibonacci(2))
-    print(fibonacci(3))
-    print(fibonacci(4))
-    print(fibonacci(5))
-    print(fibonacci(6))
-    print(fibonacci(7))
+    print(fibonacci_top_down(1), fibonacci_top_down(2), fibonacci_top_down(3),
+          fibonacci_top_down(4), fibonacci_top_down(5), fibonacci_top_down(6),
+          fibonacci_top_down(7), fibonacci_top_down(8), fibonacci_top_down(9))
+
+    print(fibonacci_bottom_up(1), fibonacci_bottom_up(2),
+          fibonacci_bottom_up(3), fibonacci_bottom_up(4),
+          fibonacci_bottom_up(5), fibonacci_bottom_up(6),
+          fibonacci_bottom_up(7), fibonacci_bottom_up(8),
+          fibonacci_bottom_up(9))
 
 
 if __name__ == '__main__':
