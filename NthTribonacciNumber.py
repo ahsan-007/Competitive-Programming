4098b2938f9bc86d09b85767c3411b2b658f3bfd
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2022-12-20 12:32:44 +0500

    Solved Problem Nth Tribonacci Number using Top Down Approach with Memoization

--- NthTribonacciNumber.py
+++ NthTribonacciNumber.py
@@ -1,4 +1,5 @@
 class Solution:
+    # Bottom Up
     def tribonacci(self, n: int) -> int:
         if n <= 2:
             return 0 if n == 0 else 1
@@ -10,5 +11,13 @@ class Solution:
             n = n - 1
         return c
 
+    # Top Down
+    def tribonacciBU(self, n: int, numbers=[0, 1, 1]):
+        if n < len(numbers):
+            return numbers[n]
+        numbers.append(self.tribonacciBU(
+            n-1, numbers) + self.tribonacciBU(n-2, numbers) + self.tribonacciBU(n-3, numbers))
+        return numbers[n]
 
-print(Solution().tribonacci(25))
+
+print(Solution().tribonacciBU(25))
