de2d29e21307b4100980e86546f1fe1abca8e2f1
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2025-12-28 12:10:52 +0500

    Count Negative Numbers in a Sorted Matrix (using binary search)

--- Daily Streak/Easy/CountNegativeNumbersInASortedMatrix.py
+++ Daily Streak/Easy/CountNegativeNumbersInASortedMatrix.py
@@ -19,7 +19,35 @@ class Solution:
             r = r - 1
         return count
 
+    def countNegativesV2(self, grid: List[List[int]]) -> int:
+        # find index of first negative element using binary search
+        def findFirstNegativeEleInd(nums, lb, ub):
+            if lb > ub:
+                return float("+inf")
+
+            mid = lb + (ub - lb) // 2
+            if nums[mid] >= 0:
+                return findFirstNegativeEleInd(nums, mid + 1, ub)
+            else:
+                return min(mid, findFirstNegativeEleInd(nums, lb, mid - 1))
+
+        count = 0
+        i = 0
+        while i < len(grid):
+            firstNegativeEleInd = findFirstNegativeEleInd(
+                grid[i], 0, len(grid[i]) - 1)
+            if firstNegativeEleInd != float("+inf"):
+                count += len(grid[i]) - firstNegativeEleInd
+            i = i + 1
+        return count
+
 
 print(Solution().countNegatives(
     grid=[[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]))
 print(Solution().countNegatives(grid=[[3, 2], [1, 0]]))
+
+print('-' * 100)
+
+print(Solution().countNegativesV2(
+    grid=[[4, 3, 2, -1], [3, 2, 1, -1], [1, 1, -1, -2], [-1, -1, -2, -3]]))
+print(Solution().countNegativesV2(grid=[[3, 2], [1, 0]]))
