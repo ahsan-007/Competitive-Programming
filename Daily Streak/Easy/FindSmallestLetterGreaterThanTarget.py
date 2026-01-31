589185beba91ad3ad359bf8b6d134dccfeedac36
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2026-01-31 21:28:29 +0500

    Find Smallest Letter Greater Than Target (Binary Search)

--- Daily Streak/Easy/FindSmallestLetterGreaterThanTarget.py
+++ Daily Streak/Easy/FindSmallestLetterGreaterThanTarget.py
@@ -12,7 +12,25 @@ class Solution:
             i = i + 1
         return letters[0]
 
+    def nextGreatestLetterV2(self, letters: List[str], target: str) -> str:
+        lb = 0
+        ub = len(letters) - 1
+        minGreatest = None
+        while lb <= ub:
+            mid = lb + (ub - lb) // 2
+            if ord(letters[mid]) > ord(target):
+                minGreatest = letters[mid]
+                ub = mid - 1
+            else:
+                lb = mid + 1
+        return minGreatest if minGreatest else letters[0]
+
 
 print(Solution().nextGreatestLetter(letters=["c", "f", "j"], target="a"))
 print(Solution().nextGreatestLetter(letters=["c", "f", "j"], target="c"))
 print(Solution().nextGreatestLetter(letters=["x", "x", "y", "y"], target="z"))
+
+print(Solution().nextGreatestLetterV2(letters=["c", "f", "j"], target="a"))
+print(Solution().nextGreatestLetterV2(letters=["c", "f", "j"], target="c"))
+print(Solution().nextGreatestLetterV2(
+    letters=["x", "x", "y", "y"], target="z"))
