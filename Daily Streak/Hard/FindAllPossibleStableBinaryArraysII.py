30cf157607e6354b578adbdf9edf0ac8ebe315be
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2026-03-10 22:28:38 +0500

    Find All Possible Stable Binary Arrays II

---
+++ Daily Streak/Hard/FindAllPossibleStableBinaryArraysII.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/find-all-possible-stable-binary-arrays-ii/description/?envType=daily-question&envId=2026-03-10
+
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        dp = [[[0, 0] for _ in range(one + 1)] for _ in range(zero + 1)]
+        for i in range(min(zero, limit) + 1):
+            dp[i][0][0] = 1
+        for j in range(min(one, limit) + 1):
+            dp[0][j][1] = 1
+
+        mod = pow(10, 9) + 7
+
+        for i in range(1, zero + 1):
+            for j in range(1, one + 1):
+                dp[i][j][0] = dp[i-1][j][0] + dp[i-1][j][1]
+                if i > limit:
+                    dp[i][j][0] = dp[i][j][0] - dp[i-limit-1][j][1]
+                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod
+
+                dp[i][j][1] = dp[i][j-1][1] + dp[i][j-1][0]
+                if j > limit:
+                    dp[i][j][1] = dp[i][j][1] - dp[i][j-limit-1][0]
+                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod
+
+
+print(Solution().numberOfStableArrays(zero=1, one=1, limit=2))
+print(Solution().numberOfStableArrays(zero=1, one=2, limit=1))
+print(Solution().numberOfStableArrays(zero=3, one=3, limit=2))
+print(Solution().numberOfStableArrays(zero=1000, one=1000, limit=800))
